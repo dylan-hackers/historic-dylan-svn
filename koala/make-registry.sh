3cd7793c30af3201605f6b8c63a562f3512715fb
Carl Gay <carlgay@gmail.com>
2004-12-19 15:30:14 +0000

    job: minor Brought make-registry.sh up-to-date.

--- koala/make-registry.sh
+++ koala/make-registry.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 if [ $# != 1 ]; then
-    echo "Usage: `basename $0` relative-path-to-koala"
+    echo "Usage: `basename $0` relative-path-to-dylan-checkout"
     echo "        The registry will be created under the current directory."
     exit 2
 fi
 
-koala_dir=$1
+dylan_root=$1
+
 
 mkdir -p registry/Sources/registry/generic
 cd registry/Sources/
-echo "abstract://dylan/../../$koala_dir/../anaphora/anaphora.lid" > registry/generic/anaphora
-echo "abstract://dylan/../../$koala_dir/../meta/meta.lid" > registry/generic/meta
-echo "abstract://dylan/../../$koala_dir/../xml-parser/xml-parser.lid" > registry/generic/xml-parser
-echo "abstract://dylan/../../$koala_dir/sources/xml-rpc-common/xml-rpc-common.lid" > registry/generic/xml-rpc-common
-echo "abstract://dylan/../../$koala_dir/sources/dylan-basics/dylan-basics.lid" > registry/generic/dylan-basics
-echo "abstract://dylan/../../$koala_dir/sources/koala/koala-unix.lid" > registry/generic/koala
-echo "abstract://dylan/../../$koala_dir/sources/koala-app/koala-app.lid" > registry/generic/koala-app
+echo "abstract://dylan/../../$dylan_root/libraries/anaphora/anaphora.lid" > registry/generic/anaphora
+echo "abstract://dylan/../../$dylan_root/libraries/meta/meta.lid" > registry/generic/meta
+echo "abstract://dylan/../../$dylan_root/libraries/xml-parser/xml-parser.lid" > registry/generic/xml-parser
+echo "abstract://dylan/../../$dylan_root/libraries/koala/sources/xml-rpc-common/xml-rpc-common.lid" > registry/generic/xml-rpc-common
+echo "abstract://dylan/../../$dylan_root/libraries/koala/sources/dylan-basics/dylan-basics.lid" > registry/generic/dylan-basics
+echo "abstract://dylan/../../$dylan_root/libraries/koala/sources/koala/koala-unix.lid" > registry/generic/koala
+echo "abstract://dylan/../../$dylan_root/libraries/koala/sources/koala-app/koala-app.lid" > registry/generic/koala-app
+echo "abstract://dylan/../../$dylan_root/libraries/koala/sources/examples/koala-basics/koala-basics.lid" > registry/generic/koala-basics
+echo "abstract://dylan/../../$dylan_root/libraries/koala/sources/examples/wiki/wiki.lid" > registry/generic/wiki
+echo "abstract://dylan/../../$dylan_root/fundev/Sources/lib/regular-expressions/regular-expressions.lid" > registry/generic/regular-expressions
+echo "abstract://dylan/../../$dylan_root/fundev/Sources/lib/string-extensions/string-extensions.lid" > registry/generic/string-extensions
